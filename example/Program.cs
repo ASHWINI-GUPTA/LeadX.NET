bd328b91dfc2364f7f3f36df9859ca7dd2e15116
Ashwini Gupta <ashwini.g@deltax.com>
2019-09-22 19:18:32 +0530

    Fix dir structure for GitHub

---
+++ example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using LeadX.NET.Entities;
+
+namespace Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Application Started...!!!");
+
+            var cred = new LeadXCredential("Your LeadX Access Token");
+
+            var leadX = LeadX.NET.LeadX.Client(cred);
+            leadX.Version = "1.0";
+
+            var org = await leadX.Organization.GetAsync();
+
+        }
+    }
+}
---
+++ src/Exceptions/LeadXErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace LeadX.NET.Exceptions
+{
+    public class LeadXErrorResponse
+    {
+        [JsonProperty(PropertyName = "errorCode")]
+        public string ErrorCode { get; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; }
+        
+        [JsonProperty(PropertyName = "validationError")]
+        public IEnumerable<ValidationError> ValidationErrors { get; set; }
+    }
+
+    public class ValidationError
+    {
+        public string Parameter { get; set; }
+        public string Message { get; set; }
+    }
+} 
